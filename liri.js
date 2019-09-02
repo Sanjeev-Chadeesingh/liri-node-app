65304ddfc3c04c1a181cb751ec90ec7d82c067e2
Leolal <leolal1984@icloud.com>
2019-09-02 18:28:00 -0400

    Added more necessities and dependencies

--- liri.js
+++ liri.js
@@ -2,4 +2,11 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var axios = require("axios");
+
+var Spotify = require("node-spotify-api");
+
+var moment = require('moment');
+moment().format();
+
 var spotify = new spotify(keys.spotify);
